4b4f253e22fb356bd8d16dfb7de0f73df6cc2b78
lenatester100 <lena.tester100@gmail.com>
2017-02-16 19:04:59 -0500

    Add files via upload

---
+++ Quadratic Program.py
@@ -0,0 +1,10 @@
+print ("Welcome to the program! Please enter the value of A, B, C, and X at the corresponding prompts")
+a = int(input("What is the value of A:  "))
+b = int(input("What is the value of B:  "))
+c = int(input("What is the value of C:  "))
+x = int(input("What is the value of X:  "))
+print ("The following quadratic was entered: ")
+result = (a*(x^2)+(b*x)+c)
+print ("The value of the quadratic is ", result)
+
+# ax2 + bx + c = 0
